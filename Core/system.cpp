3a40e8333fb20995c45577c31f91093bafba40ac
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-07-01 03:20:09 +0300

    add Core boilerplate

---
+++ Core/system.cpp
@@ -0,0 +1,8 @@
+#include "system.h"
+
+#include <iostream>
+
+System::System()
+{
+    std::cout << "Core system initialized" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <QApplication>
 
+#include "Core/system.h"
 #include "UI/mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    System *system = new System();
+
     MainWindow w;
     w.show();
     return a.exec();
